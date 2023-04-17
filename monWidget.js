0a65fb0e675e64ad75fa20ad9b92cf93f63aac5a
Louis <louis@planhub.ca>
2023-04-17 17:05:24 -0400

    test css

--- monWidget.js
+++ monWidget.js
@@ -1,30 +1,38 @@
 document.addEventListener("DOMContentLoaded", function () {
     const planhubLink = document.querySelector("a[href*='https://www.planhub.ca']");
-
     let elWidgets = document.querySelectorAll(".planhub-mobile-plans-widget");
     
     elWidgets.forEach(elWidget => {
         if (linkIsUpAndFollows(planhubLink)) {
             let newIframe = document.createElement('iframe');
             let dataWidget = elWidget.getAttribute("data-widget");
-            let url = "https://www.planhub.ca/widget/mobile-plans?uid=" + dataWidget;
+            let url = "https://www.planhub.ca/widget/mobile-plans?uid=" + dataWidget;   
             
-            //Realisation d'une requete AJAX pour verifier le nbre d'offres et adapter la taille de l'iframe
-            const xhr = new XMLHttpRequest();
-            xhr.open('GET', url);
-            xhr.onload = function () {
-                let widgetHeight = calculateWidgetHeight(xhr);
-
-                // newIframe.src = "http://localhost:85/app_dev.php/widget/mobile-plans?uid="+dataWidget;
-                newIframe.src = url;
-                newIframe.className = "widgetIframeElement";
-                newIframe.style = `width: 100%;border: 0;margin: 0 auto;display: block; height: ${widgetHeight}px`;
-                newIframe.setAttribute("data-widget", dataWidget);
+            // Fetch to verify the number of offers and adapt the size of the iframe
+            fetch(url)
+                .then(response => {
+                    if (response.ok) {
+                        return response.text();
+                    } else {
+                        throw new Error('Error: Network response was not ok');
+                    }
+                })
+                .then(data => {
+                    let widgetHeight = calculateWidgetHeight(data);
                 
-                elWidget.prepend(newIframe);
-                elWidget.removeChild(elWidget.lastElementChild);
-            };
-            xhr.send();
+                    // Set the iframe srcdoc attribute with the fetched HTML content
+                    newIframe.srcdoc = data;
+                
+                    newIframe.className = "widgetIframeElement";
+                    newIframe.style = `width: 100%; border: 0; margin: 0 auto; display: block; height: ${widgetHeight}px`;
+                    newIframe.setAttribute("data-widget", dataWidget);
+                
+                    elWidget.prepend(newIframe);
+                    elWidget.removeChild(elWidget.lastElementChild);
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
 
         } else {
             let errorMessage = document.createElement('H3');
@@ -35,24 +43,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
-
 const linkIsUpAndFollows = (link) => {
     return link && !link.getAttribute('rel')?.match(/nofollow/i);
 } 
 
-const calculateWidgetHeight = (xhr) => {
+const calculateWidgetHeight = (data) => {
     let widgetHeight = 310;
 
-    const response = xhr.responseText;
-                    
     // Transformation de la reponse
     const parser = new DOMParser();
-    const doc = parser.parseFromString(response, "text/html");
+    const doc = parser.parseFromString(data, "text/html");
 
     // Comptage du nombre d'offres, en multiple de 3
     const count = doc.getElementsByClassName("proposal_plans__box").length;
     if (count > 3) {
-        widgetHeight += Math.floor((count - 1) / 3) * 300;
+        widgetHeight += Math.floor((count - 1) / 3) * 210;
     }
 
     return widgetHeight;
